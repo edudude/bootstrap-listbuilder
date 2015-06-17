d07ebe67a7c44695a5d046a9777219c300dc6443
Jim Liddell <jim.liddell@linn.co.uk>
2015-06-17 16:53:07 +0100

    Reverted version due to aborted publish (again). Disabled git push in Gruntfile.

--- Gruntfile.js
+++ Gruntfile.js
@@ -53,6 +53,7 @@ module.exports = function (grunt) {
                 // supports reloading the pkg config var which we need
                 // as it is referenced when the nuget tasks are run
                 bump: false,
+                push: false,
                 commitMessage: 'Release <%= version %>'
                 /*github: { 
                     repo: 'linn/lawgr'
